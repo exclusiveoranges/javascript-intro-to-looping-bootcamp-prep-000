b87ffb43ab44f4a4f746f38ca4c51890f1878487
Ryan O'Toole <otoolerj@gmail.com>
2018-02-20 19:21:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,7 +10,7 @@ function makeArray() {
 }
 
 function forLoop(array) {
-  for (let i=0; i < 25; i++) {
+  for (let i=1; i < 26; i++) {
     array.push(`I am ${i} strange loops.`)
   }
   return array;
